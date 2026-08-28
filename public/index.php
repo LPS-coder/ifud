bfc4c754a12b4b331213daa8cb8b0187739de674
LPS-coder <arthur_l_garcia@estudante.sesisenai.org.br>
2026-08-28 09:25:21 -0300

    add - pagina inicial index

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>iFood - Painel Principal</title>
+</head>
+<body>
+    <h1>Sistema iFood</h1>
+    <ul>
+        <li><a href="clientes/index.php">Gerenciar Clientes</a></li>
+        <li><a href="restaurantes/index.php">Gerenciar Restaurantes</a></li>
+        <li><a href="pedidos/index.php">Gerenciar Pedidos</a></li>
+        <li><a href="pedidos/por_cliente.php">Desafio Extra: Pedidos por Cliente</a></li>
+    </ul>
+</body>
+</html>
